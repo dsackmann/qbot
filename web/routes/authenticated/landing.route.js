77948072edee0afd011848c1f73965cc18d5a3b8
Dylan Sackmann <dylan@digitalfoundry.com>
2016-01-09 13:34:19 -0800

    fix undefined middleware

--- web/routes/authenticated/landing.route.js
+++ web/routes/authenticated/landing.route.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function (app) {
-    app.get("/auth/success", checkAuth, function (req, res) {
+    app.get("/auth/success", function (req, res) {
         res.send(req.user);
     });
 };
