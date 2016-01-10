223dd105be86f9980edd8d7302b4f7b7fa45fa4c
Dylan Sackmann <dylan@digitalfoundry.com>
2016-01-09 19:08:53 -0800

    display item counts by quality

--- bot/behaviors/backpack.js
+++ bot/behaviors/backpack.js
@@ -6,13 +6,19 @@ var unirest = require("unirest");
 var mongoose = require("mongoose");
 var User = mongoose.model(require("../../schemas/User"));
 var _ = require("lodash");
+_.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
+
+var backpackInfoTmpl = _.template("{{username}} has {{numTotal}} TF2 items, including:\n\t\t" +
+    "{{numUnusual}} unusual,\n\t\t" +
+    "{{numStrange}} strange,\n\t\n" +
+    "and {{numCollectors}} collector's items.");
 
 function getItemsAsync(steamId) {
     var url = "http://api.steampowered.com/IEconItems_440/GetPlayerItems/v0001/?" +
         "key=" + config.get("steamApiKey") + "&" +
         "SteamID=" + steamId;
 
-    console.log("requesting: "  + url);
+    console.log("requesting: " + url);
     return new Promise(function (resolve, reject) {
         unirest.get(url)
             .header("Accept", "application/json")
@@ -26,17 +32,31 @@ function getItemsAsync(steamId) {
     });
 }
 
+function parseManifest(itemManifest) {
+    var counts = _(itemManifest.items)
+        .map(function (item) {
+            return item.quality
+        }).countBy().value();
+
+    return {
+        numTotal: itemManifest.items.length,
+        numUnusual: counts['5'],
+        numStrange: counts['11'],
+        numCollectors: counts['14']
+    }
+}
+
 module.exports = function (bot) {
     bot
         .on(bot.triggers['command'], 'backpack', ["@user"])
-        .do(function(bot, conf, args) {
+        .do(function (bot, conf, args) {
             console.log("backpack requested");
             var that = this;
 
             var targetUser = args.commandArgs.user;
 
             if (!targetUser) {
-               targetUser = args.message.author;
+                targetUser = args.message.author;
             }
 
             User.findOne({discordId: targetUser.id}).then(function (user) {
@@ -48,11 +68,14 @@ module.exports = function (bot) {
 
                 return getItemsAsync(user.steamId).then(function (itemManifest) {
                     if (itemManifest.status === 15) {
-                        that.reply("User's backpack is private!");
+                        that.reply(targetUser.name + "'s backpack is private!");
                         return;
                     }
 
-                    that.reply("backpack contains " + itemManifest.items.length + " items.");
+                    var templateModel = _.extend({username: targetUser.name},
+                    parseManifest(itemManifest));
+
+                    that.reply(backpackInfoTmpl(templateModel));
                 });
             }).onReject(function (err) {
                 console.error("Error fetching backpack for user: " + targetUser + ". Error: " + err);
