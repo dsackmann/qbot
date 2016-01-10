afe1250df267e7aaab0d86a3a12579764c8cbadc
Dylan Sackmann <dylansackmann@gmail.com>
2016-01-10 14:48:16 -0800

    add debug logging on server address after parsing

--- bot/services/tf2ServerService.js
+++ bot/services/tf2ServerService.js
@@ -4,36 +4,36 @@ var url = require("url");
 var Promise = require("bluebird");
 var query = require("game-server-query");
 
-function Tf2ServerService () {
+function Tf2ServerService() {
 }
 
 Tf2ServerService.prototype = {
-  getServerInfo: function (address) {
-      console.log("tf2 server service getting info for server: " + address);
+    getServerInfo: function (address) {
 
-      return new Promise(function (resolve, reject) {
-          var address = url.parse(address);
+        return new Promise(function (resolve, reject) {
+            var address = url.parse(address);
+            console.log("tf2 server service getting info for server: " + JSON.stringify(address));
 
-         query({
-             type: "tf2",
-             host: address.host,
-             port: address.port
-         }, function (response) {
-            if (response.error) {
-                reject(response);
-            } else {
-                resolve(response);
+            query({
+                type: "tf2",
+                host: address.host,
+                port: address.port
+            }, function (response) {
+                if (response.error) {
+                    reject(response);
+                } else {
+                    resolve(response);
+                }
+            });
+        }).then(function (serverInfo) {
+            return {
+                serverName: serverInfo.name,
+                map: serverInfo.map,
+                totalPlayers: serverInfo.players.length,
+                maxPlayers: serverInfo.maxplayers
             }
-         });
-      }).then(function (serverInfo) {
-          return {
-              serverName: serverInfo.name,
-              map: serverInfo.map,
-              totalPlayers: serverInfo.players.length,
-              maxPlayers: serverInfo.maxplayers
-          }
-      });
-  }
+        });
+    }
 };
 
 module.exports = new Tf2ServerService();
