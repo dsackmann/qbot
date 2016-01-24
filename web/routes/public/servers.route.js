af1e867a3d87d9bd613736f87bb3ccf1c535c425
Dylan Sackmann <dylansackmann@gmail.com>
2016-01-24 11:37:00 -0800

    fix require pathing

--- web/routes/public/servers.route.js
+++ web/routes/public/servers.route.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var tf2ServerService = require("../../lib/tf2ServerService").getInstance();
+var tf2ServerService = require("../../../lib/tf2ServerService").getInstance();
 module.exports = function (app) {
     app.get("/servers", function (req, res, next) {
         tf2ServerService.getAllServersWithPlayers().then(function (serverInfos) {
